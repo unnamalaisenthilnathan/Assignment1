8debc415da5ffa0911e3fe2f309ee042ee304a99
Unnamalai.S <Unnamalai.S@ATMECSINDT-016.ATMECS.CORP>
2019-06-11 06:26:18 -0700

    added

---
+++ encapsulation/src/main/java/javaoops/encapsulation/App.java
@@ -0,0 +1,13 @@
+package javaoops.encapsulation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
