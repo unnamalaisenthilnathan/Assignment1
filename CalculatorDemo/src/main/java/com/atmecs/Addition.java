689f1d481f606b6cabe4ab5539489f8a545eee8b
Unnamalai.S <Unnamalai.S@ATMECSINDT-016.ATMECS.CORP>
2019-06-16 22:11:18 -0700

    nexus repository

---
+++ CalculatorDemo/src/main/java/com/atmecs/Addition.java
@@ -0,0 +1,12 @@
+package com.atmecs;
+
+import com.atmecs.Calculator.Calc;
+
+public class Addition{
+	public static void main(String[] args) {
+		
+	Calc obj=new Calc();
+	int res=obj.additionOfNumbers(1,3,4,5);
+	System.out.println(res);
+}
+}
