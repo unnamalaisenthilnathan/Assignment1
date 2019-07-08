1437c1c70faf80780aeb2a72401b948f9500b4b0
unnamalai.senthilnathan <unnamalai.senthilnathan@atmecs.com>
2019-07-08 13:08:59 +0530

    reading file

--- JSON/src/JSONRead.java
+++ JSON/src/JSONRead.java
@@ -1,33 +1,47 @@
+
+
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
 
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 
-public class JSONRead {
-
-	public static void main(String[] args) throws IOException, ParseException
+public class JSONRead
+{
+	public static void main(String args[])
 	{
-		JSONParser  parser=new JSONParser();
-
-		try {
-			Object obj=parser.parse(new FileReader("JSONRead.json"));
-			JSONObject jobj=new JSONObject();
-			String name=( String) jobj.get("name");
-			System.out.println();
-		}
-	catch(FileNotFoundException e)
-	
-		{
-	e.printStackTrace();
-	
+	    JSONParser parser = new JSONParser();
+	    try
+	    {
+	        Object object = parser.parse(new FileReader("C:/fileread/reading.json"));
+	        
+	        //convert Object to JSONObject
+	        JSONObject jsonObject = (JSONObject)object;
+	        
+	        //Reading the String
+	        String name = (String) jsonObject.get("EmployeeName");
+	        Long age = (Long) jsonObject.get("EmployeeAge");
+	        
+	        //Reading the array
+	        JSONArray AddressDetails = (JSONArray)jsonObject.get("Address");
+	        
+	        //Printing all the values
+	        System.out.println("EmployeeName: " + name);
+	        System.out.println("EmployeeAge:" + age);
+	        System.out.println("Address:");
+	        for(Object primaryaddressdetails :AddressDetails)
+	        {
+	            System.out.println("\t"+primaryaddressdetails.toString());
+	        }
+	    }
+	    catch(FileNotFoundException fe)
+	    {
+	        fe.printStackTrace();
+	    }
+	    catch(Exception e)
+	    {
+	        e.printStackTrace();
+	    }
 	}
-	
-	
-	
-	
-
-}
 }
