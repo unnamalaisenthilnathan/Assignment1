8b8fa74e39916ac70d4063fab1204675bee19933
unnamalai.senthilnathan <unnamalai.senthilnathan@atmecs.com>
2019-07-25 15:52:13 +0530

    Hotel search

--- Oyo/src/main/java/OyoWebsiteAutomation/BaseClass.java
+++ Oyo/src/main/java/OyoWebsiteAutomation/BaseClass.java
@@ -7,29 +7,37 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class BaseClass {
+	
+	
+
 	public WebDriver driver;
 	public String path="Oyo.properties";
 	public Properties properties;
 	public FileInputStream fis;
 	
+	@BeforeTest
 	public void chooseBrowser() throws Exception
 	{
 		properties=new Properties();
 		fis=new FileInputStream(path);
 		properties.load(fis);
+	
 		if(properties.getProperty("browser").equalsIgnoreCase("firefox"))
 		{
 			System.setProperty("webdriver.gecko.driver", "C:\\Users\\Unnamalai.S\\Downloads\\geckodriver-v0.24.0-win64\\geckodriver.exe");
 			driver=new FirefoxDriver();
+			driver.manage().window().maximize();
 		}	
 		else if(properties.getProperty("browser").equalsIgnoreCase("chrome"))
 		{
 			System.setProperty("webdriver.chrome.driver",
 					"C:\\Users\\Unnamalai.S\\Downloads\\chromedriver_win32 (1)\\chromedriver.exe"); 
 			driver=new ChromeDriver();
+			driver.manage().window().maximize();
 		}
 		 
 		else if(properties.getProperty("browser").equalsIgnoreCase("internetexplorer"))
@@ -37,20 +45,11 @@ public class BaseClass {
 			System.setProperty("webdriver.ie.driver",
 					"C:\\Users\\Unnamalai.S\\Downloads\\IEDriverServer_x64_3.14.0\\IEDriverServer.exe"); 
 			driver=new InternetExplorerDriver();
+			driver.manage().window().maximize();
 		}
-		
-	}
-	@Test
-	public void openUrl() throws Exception
-	{
-		chooseBrowser();
 		driver.get(properties.getProperty("url"));
-	}
-	public void quitUrl() throws Exception
-	{
-		chooseBrowser();
-	    driver.quit();
-	}
+		}
+
 	
 	
 }
--- Oyo/src/main/java/OyoWebsiteAutomation/HotelSearching.java
+++ Oyo/src/main/java/OyoWebsiteAutomation/HotelSearching.java
@@ -7,17 +7,20 @@ import org.testng.annotations.Test;
 
 public class HotelSearching extends BaseClass {
 	@Test
-	public void HotelSearch()
-	{
-		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS) ;
-		driver.findElement(By.id("autoComplete__home")).click();
-		// driver.findElement(By.className("d-text16 geoSuggestionsList__locationName")).click();
-	
-		//driver.findElement(By.xpath("//span[contains(text(),'SilverKey Executive Stays 30730 Saket')]")).click();
-	
-	
-	
-	
-	
+	public void HotelSearch() {
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		driver.findElement(By.xpath("//*[@id='autoComplete__home']")).sendKeys("Gachibowli,Hyderabad,Telangana");
+		
+		driver.findElement(By.xpath("//div[@class='oyo-cell oyo-cell--12-col oyo-cell--8-col-tablet oyo-cell--4-col-phone d-text16 is-fontBold datePickerDesktop__checkInOut u-textEllipsis']")).click();
+		
+		driver.findElement(By.xpath("//div[@class='oyo-row oyo-row--no-spacing guestRoomPicker guestRoomPicker--home ']")).click();
+		
+		driver.findElement(By.xpath("//span[@class='guestRoomPickerPopUp__plus']")).click();
+		
+		
+		
+		driver.findElement(By.xpath("//button[@class='u-textCenter searchButton searchButton--home']")).click();
+		
 	}
 }
